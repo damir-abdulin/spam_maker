a8ab8f97a5f82945a929e00c1e1092b97e393ab5
Damir Abdulin <damir.abdulin@tutanota.com>
2020-06-07 11:04:38 +0300

    Refactoring.

--- function.py
+++ function.py
@@ -2,50 +2,105 @@
 #
 # GitHub: https://github.com/damir-abdulin/spam_maker.git 
 # 29/05/2020
-
 import vk_api
 
-def registration():
-    '''Если пароль не заполнен, то запрашивает вести логин и пароль.'''
-    is_enter = input("Использовать сохраненные данные?(+/-) ")
-    if is_enter == "+":
-        try:
-            with open('memory.txt','r') as memory_file:
-                memory = eval(memory_file.read())
-                login = memory['login']
-                password = memory['password']
-                return login, password
-        except:
-            print("Нет сохраненных данных для входа.\nВведите новые данные.")
-    login = input("Введите логин: ")
-    password = input("Введите пароль: ")
-    is_save = input("Сохранить логин и пароль? (+/-) ")
+def start(text='test', photo=None):
+    """
+        Отправляет пост в группы, на которые подписан пользователь.
+        Получает данные про запись, а возвращает
+        информацию о том, где Ззапись размещена и номер этого поста.
         
-    if is_save == "+":
-        with open('memory.txt', 'w') as memory_file:
-            data = {'login': login, 'password': password}
-            memory_file.write(str(data))
-            
-    return login, password
-
-def start_work(login, password):
-    '''Начинает работу.'''
+        Аргументы:
+        text (str) -- текст записи. 
+        photo (str) -- фотография, которую нужно прикрепить к записи.
+            Требует свой id в виде 'photo598071478_457239148'
+    """
+    vk = start_work()
+    groups = get_groups(vk)
+    send_post(vk, groups, text, photo)
+    
+def start_work():
+    """
+        Открывает доступ к аккаунту.
+        Запрашивает данные для входа и если эти данные верны,
+        то возвращает рабочую сессию ВК.
+    """
     while True:
         try:
+            login = input("Введите логин: ")
+            password = input("Введите пароль: ")
+            # Регистрация пользователя.
             session = vk_api.VkApi(login, password)
+            session.auth()
             break
         except:
-            print("В доступе отказанно.")
-    
-    # Настройка аккаунта
-    session.auth()
+            print("Неверный пароль или логин. Повторите попытку")
     vk = session.get_api()
+             
+    return vk
     
-    return session, vk
+def get_groups(vk):
+    """
+        Возвращает все группы пользователя в виде списка.
+        
+        Аргументы:
+        vk -- информация для работы с vk_api. Получается с помощью функции
+            start_work
+    """
+    user_id = vk.users.get()[0]["id"]
+    groups_full = vk.groups.get(user_id=vk.users.get()[0]["id"])
     
-def get_group(vk):
-    '''Возвращает список групп.'''
-    groups_full = vk.groups.get(user_id='592642016')
+    # Получаем id всех групп.
     groups = groups_full['items']
-
+   
     return groups
+    
+def send_post(vk, groups, text, photo):
+    """
+        Отправляет записи во все группы из groups и выводит информацию
+        о выполненной работе.
+        
+        Аргументы:
+        vk -- информация для работы с vk_api. Получается с помощью функции
+            start_work
+        groups -- спиоск групп, в которые нудно отправить запись.
+        text (str) -- текст записи. 
+        photo (str) -- фотография, которую нужно прикрепить к записи.
+            Требует свой id в виде 'photo598071478_457239148'
+    """
+    for group in groups:
+        try:
+            post = vk.wall.post(owner_id=int(-(group)),
+                message=text, attachments=photo)
+            print_result(vk, post, group, True)
+        except:
+            print_result(vk, post, group, False)
+
+def print_result(vk, post, group, flag):
+    """
+        Выводит отредактированную на экран информацию о группе,
+        в которую выложили пост, и о номера поста в данной группе в 
+        удобном для чтении виде.
+        
+        Аргументы:
+            post -- инофрмация об отпраленной записи.
+            group -- группа, в которую отправлен пост.
+            flag -- информация об успешности отправления записи.
+                True -- успех, а False -- ошибка.
+    """
+    import colorama
+    from colorama import Fore, Style
+    
+    colorama.init()
+
+    if flag:
+        print(Fore.GREEN + "Успешно отправлено")
+        print(Style.RESET_ALL + "Группа: " +
+            vk.groups.getById(group_id = group)[0]["name"] + "")
+        print("Запись: " + str(post["post_id"]))
+        print("----------------")
+    else:
+        print(Fore.RED + "Упс, что-то пошло не так.")
+        print(Style.RESET_ALL + "Группа: " +
+            vk.groups.getById(group_id = group)[0]["name"] + "")
+        print("----------------")
--- spam_maker.py
+++ spam_maker.py
@@ -2,20 +2,10 @@
 # Программа получает список групп и запись, которую необходимо отправить.
 #
 # GitHub: https://github.com/damir-abdulin/spam_maker.git 
-# 23/05/2020
-
-import vk_api
-import colorama
-
-from colorama import Fore, Style
 
 import function as f
 
-login, password = f.registration()
-session, vk = f.start_work(login, password)
-
-# Оформление записи.
-message = (
+text = (
     'Группа компаний "Серволюкс" объявляет конкурс на вакансию РАБОЧИХ БЕЗ ОПЫТА РАБОТЫ' +
     '\n\nРабочее место - ЗАО "Серволюкс Агро", аг. Межисетки,'+
     '\n\nЖдем Ваше резюме!'+
@@ -23,20 +13,6 @@ message = (
     '\n\nРезюме просьба направлять на адрес: irina.abdulina@servolux.by'+
     '\n\n#Servolux #Серволюкс #работа #Могилев #Межисетки #Карьера #servoluxcareer'
     )
-photo = 'photo598071478_457239148'
+photo = 'photo592642016_457239148'
 
-# Для вывода цветного текста.
-colorama.init()
-
-groups = f.get_group(vk)
-for group in groups:
-    try:
-        post = vk.wall.post(owner_id=int(-(group)), message=message, attachments=photo)
-        print(Fore.GREEN + "Успешно отправлено")
-        print(Style.RESET_ALL + "Группа: " + vk.groups.getById(group_id = group)[0]["name"] + "")
-        print("Пост: " + str(post["post_id"]))
-        print("----------------")
-    except:
-        print(Fore.RED + "Упс, что-то пошло не так.")
-        print(Style.RESET_ALL + "Группа: " + vk.groups.getById(group_id = group)[0]["name"] + "")
-        print("----------------")
+f.start(text, photo)
